93c76e7bc3b6e392ff0163c7274c9d43c218ad1d
SeanClosson <digisilk@yahoo.com>
2025-08-14 23:12:25 +0000

    changes to tests

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from "@playwright/test";
 
 test("Jekyll homepage renders correctly", async ({ page }) => {
-  await page.goto("http://localhost:4000");
+  await page.goto("/");
   await page.screenshot({ path: "homepage.png" });
 
-  // Optional: check for a known element
   await expect(page.locator("h1")).toBeVisible();
+
+  await expect(page.locator("body")).toContainText(/closson/i);
 });
